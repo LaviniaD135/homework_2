29f0c0b240c7604b6f889f095893dde261c98344
LaviniaD135 <63166074+LaviniaD135@users.noreply.github.com>
2020-11-28 19:23:24 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,9 +1,23 @@
 function addTokens(input, tokens){
+    if(typeof(input) != "string") throw new Error('Invalid array format');
+    if(input.length < 6) throw new Error('Input should have at least 6 characters');
+    if(!input.includes("...")) return input;
+     
     
-}
-
-const app = {
-    addTokens: addTokens
-}
-
-module.exports = app;
+    tokens.forEach(token => {
+     let resultValue ="${x}"
+     let [value] = Object.values(token);
+     resultValue = resultValue.replace("x",value);
+     let result = input.replace("...",resultValue);
+     input = result;
+    });
+    return input;
+ }
+ 
+ const app = {
+     addTokens: addTokens
+ }
+ 
+ module.exports = app;
+ 
+ 
